97ed8cab58da992f41a59fa17b5f446c1aafa317
Tejas Kannan <tkannan@berkeley.edu>
2016-08-24 22:48:04 -0700

    Added comments to problem 4

--- Problem4.java
+++ Problem4.java
@@ -2,12 +2,17 @@ import java.lang.Math;
 
 public class Problem4 {
 
+	public static final int UPPER = 999;
+	public static final int LOWER = 100;
 
 	public static void main(String[] args) {
-		System.out.println(getLargestPalindrome(100 * 100, 999 * 999));
+		System.out.println(getLargestPalindrome(LOWER * LOWER, UPPER * UPPER));
 		// Answer: 906609
 	}
 
+	/*
+	 * Returns whether or not the given integer is a palindrome
+	 */
 	private static boolean isPalindrome(int x) {
 		String s = String.valueOf(x);
 		int len = s.length();
@@ -20,16 +25,23 @@ public class Problem4 {
 	}
 
 	private static int getLargestPalindrome(int lower, int upper) {
+		// For each palindrome between the upper and lower limits, we see if it has two three-digit factors
+		// if so, then the first one returned will be the largest palindrome with two three-digit factors
+		// because we are starting at the upper limit
 		for (int i = upper; i >= lower; i--) {
-			if (isPalindrome(i) && hasTwoThreeDigitFactors(i)) {
+			if (isPalindrome(i) && hasTwoOrThreeDigitFactor(i)) {
 				return i;
 			}
 		}
 		return 0;
 	}
 
-	private static boolean hasTwoThreeDigitFactors(int n) {
-		for (int i = 100; i < 999; i++) {
+	private static boolean hasTwoOrThreeDigitFactor(int n) {
+		for (int i = LOWER; i < UPPER; i++) {
+
+			// If n is divisible by i and (n / i) is a three digit number, then
+			// n is the product of two three digit numbers, as i is a three-digit 
+			// number by construction
 			if ((n % i == 0) && (isThreeDigits(n / i))) {
 				return true;
 			}
@@ -38,7 +50,7 @@ public class Problem4 {
 	}
 
 	private static boolean isThreeDigits(int n) {
-		return n >= 100 && n < 1000;
+		return n >= LOWER && n <= UPPER;
 	}
 
 }
