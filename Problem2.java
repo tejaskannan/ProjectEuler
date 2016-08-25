167be167da3fb38bf6365166c2f82cf695f01a3d
Tejas Kannan <tkannan@berkeley.edu>
2016-08-24 22:39:44 -0700

    Added comments

--- Problem2.java
+++ Problem2.java
@@ -11,12 +11,19 @@ public class Problem2 {
 	 */
 	public static int sumOfEvenFibonacci(int limit) {
 		int sum = 0;
+
+		// start the sequence with 1 and 2
 		int curr = 1;
 		int next = 2;
+
 		while (curr <= limit) {
+
+			// If the fibonacci number is even, then add it to our sum counter
 			if (curr % 2 == 0) {
 				sum += curr;
 			}
+
+			// Find the next fibonacci number
 			int temp = next;
 			next += curr;
 			curr = temp;
