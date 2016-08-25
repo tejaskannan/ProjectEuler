bff486569b6cec576f97dc6a91aeb157c07c2d46
Tejas Kannan <tkannan@berkeley.edu>
2016-08-24 22:37:27 -0700

    Reduced unecessary logic

--- Problem1.java
+++ Problem1.java
@@ -13,11 +13,15 @@ public class Problem1 {
 	public static int sumMultiples(int x, int y, int limit) {
 		int sum = 0;
 		for (int i = 1; i < limit; i++) {
-			int xMod = i % x;
-			int yMod = i % y;
-			if ((xMod == 0 && yMod == 0) || (xMod == 0 && yMod != 0) || (xMod != 0 && yMod == 0)) {
+			int xMod = i % x; // computes the current number modulo x
+			int yMod = i % y; // computes the current number modulo y
+
+			// if the current number is divisible by x or y (or both), then add the number
+			// to the running sum
+			if (xMod == 0 || yMod == 0) {
 				sum += i;
 			}
+
 		}
 		return sum;
 	}
